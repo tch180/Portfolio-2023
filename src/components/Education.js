42bc08ce9bae64b4b34d9da161dc4976e557eb4e
tch180 <thopstadredner@gmail.com>
2023-05-01 14:26:23 -0400

    update responsiveness for education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -41,11 +41,11 @@ const Education = () => {
       <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
         Education
       </h2>
-      <div ref={ref} className="w-[75%] mx-auto relative ">
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div  style={{scaleY: scrollYProgress}}
-        className="absolute left-8 top-0 w-[4px] h-full bg-light origin-top dark:bg-dark" />
+        className="absolute left-8 top-0 w-[4px] h-full bg-light origin-top dark:bg-dark md:w-[2px] md:left-[30px] xs:left-[20px] "/>
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
 
 
           <Details
