27ddd3e797582b2462f2e2edb172b87e0470ec0c
tch180 <thopstadredner@gmail.com>
2023-04-30 21:28:30 -0400

    updated exp to fix alignment

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -49,7 +49,7 @@ const Experience = () => {
       </h2>
       <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full  ">
         <motion.div  style={{scaleY: scrollYProgress}}
-        className="absolute left-9 top-0 w-[4px] h-full bg-light origin-top dark:bg-dark md:w-[2px] md:left-[30px] xs:left-[20px]" />
+        className="absolute left-8 top-0 w-[4px] h-full bg-light origin-top dark:bg-dark md:w-[2px] md:left-[30px] xs:left-[20px]" />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
