57f18f51ff3d1f0114b0d8818d753e27b597ee3c
tch180 <thopstadredner@gmail.com>
2023-05-01 14:22:24 -0400

    updated education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -38,8 +38,8 @@ const Education = () => {
   );
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">
-        Expereince
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Education
       </h2>
       <div ref={ref} className="w-[75%] mx-auto relative ">
         <motion.div  style={{scaleY: scrollYProgress}}
